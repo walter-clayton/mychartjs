089881e2e43f3ce7b980ecbb6a50cb2a572ea526
Walter Clayton <walthedude@gmail.com>
2020-02-20 00:32:38 +0100

    fixed the flash connect

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,15 +11,12 @@ router.get("/", (req, res) => {
             console.log("here are all of the scores...");
             console.log(documents);
             console.log(documents[0].squat);
-            req.flash("success", "you added a score!");
-            res.render("index", {documents: documents, message: req.flash("success")});
-            
-          
+            res.render("index", {documents: documents, message: req.flash("info") });
         }
     });
 });
 
-router.post("/scores", (req, res) => {
+router.post("/", (req, res) => {
     var myData = new Score({
         squat : req.body.squat,
         benchPress : req.body.benchPress,
@@ -27,8 +24,8 @@ router.post("/scores", (req, res) => {
     })
     myData.save()
         .then(item => {
-            req.flash("info", "Hi there!");
             console.log("data saved");
+            req.flash('info', 'Score added!')
             res.redirect("/"); 
 
         })
@@ -40,18 +37,10 @@ router.post("/scores", (req, res) => {
 
 
 
-
-// find the score
-router.get("/:id", function(req, res){
-    Score.findById(req.params.id, function(err, documents){
-        res.render("scores/show", {documents: documents});
-    });
-});
-
 // EDIT CAMPGROUND ROUTE
 router.get("/:id/edit", function(req, res){
     Score.findById(req.params.id, function(err, documents){
-        res.render("scores/edit", {documents: documents});
+        res.render("scores/edit", {documents: documents, message: req.flash("info")});
     });
 });
 
@@ -63,8 +52,8 @@ router.put("/:id", function(req, res){
            res.send("There was an error updating your score");
        } else {
            //redirect somewhere(show page)
-           req.flash("info", "Hi there!");
-           res.redirect("/" + req.params.id);
+           req.flash('info', 'score updated!')
+           res.redirect("/");
        }
     });
 });
@@ -76,8 +65,8 @@ router.delete("/:id", function(req, res){
        if(err){
            res.send("there was a problem");
        } else {
-            req.flash('test', 'it worked');
-            res.redirect("/");
+        req.flash('info', 'score deleted!')
+        res.redirect("/");
        }
     });
  });
--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(session({
 }));
 app.use(flash());
 
+
 // Connect to the model 
 var db = require('./backend/config/database');
 // Connect to the database
